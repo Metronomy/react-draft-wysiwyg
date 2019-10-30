a29ad3ce71d28d174e2726b15f7da5af199035b1
Angelo <Metronomy@users.noreply.github.com>
2019-10-30 16:01:43 +0000

    Update index.js

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -450,7 +450,7 @@ export default class WysiwygEditor extends Component {
   };
 
   preventDefault: Function = (event: Object) => {
-    if (event.target.tagName === "INPUT" || event.target.tagName === "LABEL" || event.target.tagName === "TEXTAREA") {
+    if (event.target.tagName === "INPUT" || event.target.tagName === "LABEL" || event.target.tagName === "TEXTAREA" || event.target.className === "DraftEditor-root") {
       this.focusHandler.onInputMouseDown();
     } else {
       event.preventDefault();
